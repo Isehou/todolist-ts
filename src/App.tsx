c64e8742f6ae0c00d1b2c351f351852d5014170c
isehou <zaku47shift@gmail.com>
2023-08-08 22:33:36 +0600

    rerendering filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Todolist } from "./components/Todolist";
 
 function App() {
+  let initTasks = [
+    { id: 1, title: "HTML", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "React", isDone: false },
+    { id: 4, title: "Vue", isDone: true },
+  ];
+
+  let arr = useState(initTasks);
+  let tasks = arr[0];
+  let setTasks = arr[1];
+  function removeTask(id: number) {
+    let filteredTasks = tasks.filter((t) => {
+      return t.id !== id;
+    });
+    setTasks(filteredTasks);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title="What to learn" tasks={tasks} removeTask={removeTask} />
     </div>
   );
 }
